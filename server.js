c320898806860968ed6fe22523f4e51239ed9b12
Zheng <williamsjr.nexus@gmail.com>
2016-08-04 21:01:04 +0000

    checked

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express=require("express");
 var app=express();
-app.get('/',function(req,res){
-    res.end('Hello World!');
+app.get('/:date',function(req,res){
+    if(/^\d+/.test(req.params.date)) req.params.date=parseInt(req.params.date)*1000;
+    var date=new Date(req.params.date);
+    if(date.getTime()!==date.getTime()) {
+        var result = {  unix:null,natural:null};
+        
+    }
+    else{
+        var options = {  year: 'numeric', month: 'long', day: 'numeric' };
+        var result = {  unix:Math.round(date.getTime()/1000),natural:date.toLocaleDateString('en-US', options)};
+    }
+    res.json(result);
 });
 app.listen(8080,function(){
     console.log("listen to port : 8080");
